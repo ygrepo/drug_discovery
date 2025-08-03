22678e20bf43b416a11f3b4d560826ef195d8402
Yves Greatti <ygrepo@gmail.com>
2025-08-03 17:53:45 -0400

    Updates

--- run/extract_embedding.sh
+++ run/extract_embedding.sh
@@ -57,7 +57,6 @@ while [[ $# -gt 0 ]]; do
     --log_dir) LOG_DIR="$2"; shift 2 ;;
     --log_level) LOG_LEVEL="$2"; shift 2 ;;
     --seed) SEED="$2"; shift 2 ;;
-    --log-level) LOG_LEVEL="$2"; shift 2 ;;
     *) echo "Unknown parameter: $1"; exit 1 ;;
   esac
 done
