4dcf261104eeb9cd8a5fb9ee638935997ece3cfe
Yves Greatti <ygrepo@gmail.com>
2025-08-06 13:30:48 -0400

    New bash script to create conda env

--- qsubs/activate_conda_env.sh
+++ qsubs/activate_conda_env.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+#BSUB -J ativate_conda_env
+#BSUB -P acc_DiseaseGeneCell
+#BSUB -q gpu
+#BSUB -gpu "num=1"
+#BSUB -R h100nvl
+#BSUB -n 1
+#BSUB -R "rusage[mem=32000]"
+#BSUB -W 0:30
+#BSUB -o logs/activate_conda_env.%J.out
+#BSUB -e logs/activate_conda_env.%J.err
+
+set -euo pipefail
+
 module purge
 module load cuda/11.8 cudnn
 module load anaconda3/latest
