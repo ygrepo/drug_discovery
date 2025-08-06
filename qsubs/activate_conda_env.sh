7555f3f31c6c3b7ed5e8a0b86704a20cfdabc1d4
Yves Greatti <ygrepo@gmail.com>
2025-08-06 13:33:37 -0400

    New bash script to create conda env

--- qsubs/activate_conda_env.sh
+++ qsubs/activate_conda_env.sh
@@ -24,3 +24,29 @@ export CONDARC="$PROJ/conda/condarc"
 conda activate /sc/arion/projects/DiseaseGeneCell/Huang_lab_data/.conda/envs/drug_discovery_env
 
 ml proxies/1 || true
+
+
+export HF_HOME="/sc/arion/projects/DiseaseGeneCell/Huang_lab_data/.cache/huggingface"
+mkdir -p "$HF_HOME"
+MODEL_NAME="/sc/arion/projects/DiseaseGeneCell/Huang_lab_data/models/esm1v_t33_650M_UR90S_5"
+
+ ------------------ Torch + Model Test ----------------
+python <<'PYCODE'
+import os, torch
+from transformers import AutoModel
+
+print("="*40)
+print("✅ PyTorch version:", torch.__version__)
+print("✅ CUDA version:", torch.version.cuda)
+print("✅ GPU available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("✅ GPU device:", torch.cuda.get_device_name(0))
+print("="*40)
+
+model_path = os.environ.get("MODEL_NAME")
+print(f"Attempting to load model from: {model_path}")
+model = AutoModel.from_pretrained(model_path, local_files_only=True).eval()
+print("✅ Model loaded successfully")
+
+print("✅ All tests passed!")
+PYCODE
