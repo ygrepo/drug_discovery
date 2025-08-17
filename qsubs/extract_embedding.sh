f2fa11f436df9560c0fc91d9c371c8aef8c45975
Yves Greatti <ygrepo@gmail.com>
2025-08-17 16:21:29 -0400

    Updating script for extracting embeddings

--- qsubs/extract_embedding.sh
+++ qsubs/extract_embedding.sh
@@ -31,9 +31,9 @@ export HF_HOME="/sc/arion/projects/DiseaseGeneCell/Huang_lab_data/.cache/hugging
 mkdir -p "$HF_HOME"
 
 # Must export for Python to see it
-#MODEL_TYPE="ESM2"
+MODEL_TYPE="ESM2"
 #MODEL_TYPE="ESMv1"
-MODEL_TYPE="MUTAPLM"
+#MODEL_TYPE="MUTAPLM"
 #MODEL_TYPE="ProteinCLIP"
 
 
@@ -42,8 +42,8 @@ DATA_FN="mutadescribe_data/structural_split/train.csv"
 OUTPUT_DIR="output/data"
 #OUTPUT_FN="${OUTPUT_DIR}/esm1v_structural_split_train_with_embeddings.csv"
 #OUTPUT_FN="${OUTPUT_DIR}/esmv1_structural_split_train_with_embeddings.csv"
-#OUTPUT_FN="${OUTPUT_DIR}/esm2_t33_650M_UR50D_structural_split_train_with_embeddings.csv"
-OUTPUT_FN="${OUTPUT_DIR}/mutaplm_structural_split_train_with_embeddings.csv"
+OUTPUT_FN="${OUTPUT_DIR}/esm2_t33_650M_UR50D_structural_split_train_with_embeddings.csv"
+#OUTPUT_FN="${OUTPUT_DIR}/mutaplm_structural_split_train_with_embeddings.csv"
 #OUTPUT_FN="${OUTPUT_DIR}/proteinclip_structural_split_train_with_embeddings.csv"
 N=2000
 LOG_DIR="logs"
