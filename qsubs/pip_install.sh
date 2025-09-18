15d2f8e6da49569f250ed639d2f525ec286d943f
Yves Greatti <ygrepo@gmail.com>
2025-09-18 15:40:21 -0400

    Updates

--- qsubs/pip_install.sh
+++ qsubs/pip_install.sh
@@ -25,27 +25,27 @@ conda activate "${ENV_PREFIX}"
 
 # --- Use the env's Python and pip ---
 PYTHON="${ENV_PREFIX}/bin/python"
-
+PIP="${ENV_PREFIX}/bin/pip"
 # Upgrade pip inside the env (safe)
-"${PYTHON}" -m pip install --upgrade pip
+#"${PYTHON}" -m pip install --upgrade pip
 
-# --- Install PyTorch built for CUDA 12.x ---
-CUDA_INDEX_URL="https://download.pytorch.org/whl/cu124"   # switch to cu121 if your cluster is CUDA 12.1
-"${PYTHON}" -m pip install torch torchvision torchaudio --index-url "${CUDA_INDEX_URL}"
+# # --- Install PyTorch built for CUDA 12.x ---
+# CUDA_INDEX_URL="https://download.pytorch.org/whl/cu124"   # switch to cu121 if your cluster is CUDA 12.1
+# "${PYTHON}" -m pip install torch torchvision torchaudio --index-url "${CUDA_INDEX_URL}"
 
-# --- Common libs ---
-"${PYTHON}" -m pip install transformers safetensors accelerate
-"${PYTHON}" -m pip install pandas numpy tqdm
+# # --- Common libs ---
+# "${PYTHON}" -m pip install transformers safetensors accelerate
+# "${PYTHON}" -m pip install pandas numpy tqdm
 
-# --- Sanity checks ---
-"${PYTHON}" - <<'PY'
-import os, sys, torch
-print("Python:", sys.version.split()[0])
-print("Torch:", torch.__version__,
-      "Built for CUDA:", torch.version.cuda,
-      "CUDA available:", torch.cuda.is_available())
-print("PIP_CACHE_DIR:", os.environ.get("PIP_CACHE_DIR"))
-print("CONDA_PKGS_DIRS:", os.environ.get("CONDA_PKGS_DIRS"))
-from transformers import AutoModel
-print("Transformers import: OK")
-PY
+# # --- Sanity checks ---
+# "${PYTHON}" - <<'PY'
+# import os, sys, torch
+# print("Python:", sys.version.split()[0])
+# print("Torch:", torch.__version__,
+#       "Built for CUDA:", torch.version.cuda,
+#       "CUDA available:", torch.cuda.is_available())
+# print("PIP_CACHE_DIR:", os.environ.get("PIP_CACHE_DIR"))
+# print("CONDA_PKGS_DIRS:", os.environ.get("CONDA_PKGS_DIRS"))
+# from transformers import AutoModel
+# print("Transformers import: OK")
+# PY
